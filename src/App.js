98a71e2cd13b27f47f5bf5ef4ece7296735a0fc9
Aaron Paden <aaronbpaden@gmail.com>
2022-04-18 13:54:42 -0500

    initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main className="main">
+        <section className="main-header">
+          <h1>Todo</h1>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
