551a5453ea3e6d121291898b897aeaf9b1e739ef
Aaron Paden <aaronbpaden@gmail.com>
2022-04-18 21:31:36 -0500

    Fix addItem thanks to input from Ethan.

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -2,21 +2,23 @@ import { useState } from 'react';
 const TodoItems = () => {
 	const [items, setItems] = useState([]);
 	const [itemText, setItemText] = useState("");
+	const handleChange = (e) => {
+		setItemText(e.target.value);
+		console.log(itemText);
+	};
 	const addItem = () => {
-		setItemText(document.getElementById('textBox').value);
 		setItems(prev => [
 			...prev,
 			<div key={prev.length+1} className="todo-item">
 				{itemText}
 			</div>
 		]);
-		document.getElementById('textBox').value='';
 	}
 	return(
 		<section className="todo-items">
 			{items}
 			<div className="todo-item todo-adder">
-				<input id="textBox" type="text" placeholder="enter item" />
+				<input value={itemText} onChange={handleChange} id="textBox" type="text" placeholder="enter item" />
 				<button onClick={addItem}>+</button>
 			</div>
 		</section>
